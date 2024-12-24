a9d715ee64dd9c7e467bb2893d631559a1c10a89
Eduardo Mejia <mejiaeduardo@yahoo.com>
2024-12-23 20:31:46 -0500

    add events for sliders

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,19 +1,19 @@
-import React from "react";
+import { React, useState } from "react";
 import { assets, projectsData } from "../assets/assets";
 
 const Projects = () => {
-    const [currentIndex, setcurrentIndex] = useState(0);
-    const [cardsToShow, setcardsToShow] = useState(1);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [cardsToShow, setcardsToShow] = useState(1);
 
-    const nextProject = () => {
-      setcurrentIndex((prevIndex) => (prevIndex + 1) %
-       projectsData.length);
-    }
-    const prevProject = () => {
-      setcurrentIndex((prevIndex) => prevIndex === 0 ?
-       projectsData.length - 1 : prevIndex - 1)
-    }
-  }
+  const nextProject = () => {
+    setCurrentIndex((prevIndex) => prevIndex + 1) % projectsData.length;
+  };
+  const prevProject = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? projectsData.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <div
       className="container mx-auto py-4 pt-20
@@ -39,16 +39,16 @@ const Projects = () => {
       {/* slider buttons */}
       <div className="flex justify-end items-center mb-8">
         <button
-        onClick={prevProject}
           className="p-3 bg-gray-200 rounded mr-2"
           aria-label="Previous Project"
+          onClick={prevProject}
         >
           <img src={assets.left_arrow} alt="Previous" />
         </button>
         <button
-         onClick={nextProject}
           className="p-3 bg-gray-200 rounded mr-2"
           aria-label="Next Project"
+          onClick={nextProject}
         >
           <img src={assets.right_arrow} alt="Next" />
         </button>
@@ -59,6 +59,9 @@ const Projects = () => {
         <div
           className="flex gap-8 transition-transform
           duration-500 ease-in-out"
+          style={{
+            transform: `translateX(-${(currentIndex * 100) / cardsToShow}%)`,
+          }}
         >
           {projectsData.map((project, index) => (
             <div
