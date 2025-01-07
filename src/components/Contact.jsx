c3250a4f2e7f3a69786ecef61d170efd5f66d35b
Eduardo Mejia <mejiaeduardo@yahoo.com>
2025-01-07 08:55:36 -0500

    style contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
     setResult("Sending....");
     const formData = new FormData(event.target);
 
-    formData.append("access_key", "YOUR_ACCESS_KEY_HERE");
+    formData.append("access_key", "cda15fa2-aae4-4bbb-9997-a72cd9ba2c1b");
 
     const response = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
@@ -18,11 +18,13 @@ const Contact = () => {
     const data = await response.json();
 
     if (data.success) {
-      setResult("Form Submitted Successfully");
+      setResult("");
+      alert("Form Submitted Successfully");
       event.target.reset();
     } else {
       console.log("Error", data);
-      setResult(data.message);
+      alert(data.message);
+      setResult("");
     }
   };
 
@@ -77,6 +79,7 @@ const Contact = () => {
         <div className="my-6 text-left">
           Message
           <textarea
+            name="Message"
             className="w-full border border-300 rounded py-3
             px-4 mt-2 h-48 resize-none"
             placeholder="Message"
@@ -84,7 +87,7 @@ const Contact = () => {
           ></textarea>
         </div>
         <button className="bg-blue-600 text-white py-2 px-12 mb-10 rounded">
-          Send Message
+          {result ? result : "Send Message"}
         </button>
       </form>
     </div>
