b9a55c5b190c8188ef8502ee1c3be54a49fd3110
Eduardo Mejia <mejiaeduardo@yahoo.com>
2025-01-08 14:39:47 -0500

    finish styling footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -45,9 +45,25 @@ const Footer = () => {
           <p className="text-gray-400 mb-4 max-w-80">
             The latest news, articles, resources sent to your inbox weekly.
           </p>
+          <div className="flex gap-2">
+            <input
+              type="email"
+              placeholder="Enter email"
+              className="p-2 rounded bg-gray-800 text-gray-400 border
+              border-gray-700 focus:outline-none w-full md:w-auto"
+            />
+            <button className="py-2 px-4 rounded bg-blue-500 text-white">
+              Subscribe
+            </button>
+          </div>
         </div>
       </div>
-      <div></div>
+      <div
+        className="border-t border-gray-700 py-4 mt-10 text-center
+        text-gray-500"
+      >
+        Copyright 2024 All Rights Reserved.
+      </div>
     </div>
   );
 };
